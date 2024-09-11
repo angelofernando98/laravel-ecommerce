f1a413f5897589d5825843dc24749a63526d2791
Angelo <angelo.fernando2pac@gmail.com>
2024-09-12 00:48:19 +0530

    Updated Order model with mass-assignable attributes and product relationship

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id', 'grand_total', 'payment_method', 'payment_status', 'status', 'currency', 'shipping_amount', 'shipping_method', 'notes'];
+
+    // Get the user that owns the Order
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    // Get all of the order items for the Order
+    public function Items(){
+        return $this->hasMany(OrderItem::class);
+    }
+
+    // Get the address that owns the Order
+    public function address(){
+        return $this->belongsTo(Address::class);
+    }
 }
