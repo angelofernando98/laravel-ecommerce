59ed707f829a24286fe88a6c66dfa0b0fbd5a31f
Angelo <angelo.fernando2pac@gmail.com>
2024-09-11 23:59:14 +0530

    Add columns to the table where there are Addresses of the Orders

--- database/migrations/2024_09_11_113325_create_addresses_table.php
+++ database/migrations/2024_09_11_113325_create_addresses_table.php
@@ -11,8 +11,20 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // Create the addresses table
+        // This table stores the addresses of the orders
+        // The address of an order is the shipping address
+        // Each address has an id, an order id (foreign key to the orders table), a first name, a last name, a phone, a street address, a city, a state, and a zip code
         Schema::create('addresses', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('order_id')->constrained('orders')->cascadeOnDelete();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('phone')->nullable();
+            $table->text('street_address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('state')->nullable();
+            $table->string('zip_code')->nullable();
             $table->timestamps();
         });
     }
