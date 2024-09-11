0d67573ebe2f3fcd2ecf8fbd44785736c0cfac29
Angelo <angelo.fernando2pac@gmail.com>
2024-09-11 23:54:50 +0530

    Add columns to the Order Items table

--- database/migrations/2024_09_11_113217_create_order_items_table.php
+++ database/migrations/2024_09_11_113217_create_order_items_table.php
@@ -11,8 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // Create the order items table
+        // Each order item has an id, an order id (foreign key to the orders table), a product id (foreign key to the products table), a quantity, a unit amount, a total amount, and timestamps (created_at and updated_at)
         Schema::create('order_items', function (Blueprint $table) {
             $table->id();
+            $table->foreingId('order_id')->constrained('orders')->cascadeOnDelete();
+            $table->foreingId('product_id')->constrained('products')->cascadeOnDelete();
+            $table->integer('quantity')->default(1);
+            $table->decimal('unit_amount', 10, 2)->nullable();
+            $table->decimal('total_amount', 10, 2)->nullable();
             $table->timestamps();
         });
     }
