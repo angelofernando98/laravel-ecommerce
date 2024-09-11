711df3d10e89e03f0e26127a88a51a59e9dd82b0
Angelo <angelo.fernando2pac@gmail.com>
2024-09-12 01:00:40 +0530

    Updated Address model with mass-assignable attributes and product relationship

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Address extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['order_id', 'first_name', 'last_name', 'phone', 'street_address', 'city', 'state', 'zip_code'];
+
+    // Get the order that owns the Address
+    public function order(){
+        return $this->belongsTo(Order::class);
+    }
+
+    // Get the full address of the user, which is the first name and last name concatenated with a space in between.
+    // Example: John Doe
+    public function getFullAddressAttribute(): string
+    {
+        return "{$this->first_name} {$this->last_name}";
+    }
 }
