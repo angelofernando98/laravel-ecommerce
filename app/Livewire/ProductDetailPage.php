48ac5cc37b22edfa07d9908693ce34f09970aa16
Angelo <angelo.fernando2pac@gmail.com>
2024-09-18 16:48:54 +0530

    Product Detail Page made Dynamic

--- app/Livewire/ProductDetailPage.php
+++ app/Livewire/ProductDetailPage.php
@@ -2,12 +2,23 @@
 
 namespace App\Livewire;
 
+use App\Models\Product;
 use Livewire\Component;
 
 class ProductDetailPage extends Component
 {
+
+    // Get the product slug from the URL and store it in the component's state.
+    public $slug;
+
+    public function mount($slug) { 
+        $this->slug = $slug;
+    }
+
     public function render()
     {
-        return view('livewire.product-detail-page');
+        return view('livewire.product-detail-page', [
+            'product' => Product::where('slug', $this->slug)->firstOrFail(),
+        ]);
     }
 }
