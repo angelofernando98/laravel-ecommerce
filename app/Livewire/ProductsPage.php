47f67ee4df20e635d4a18a142186878b409ed555
Angelo <angelo.fernando2pac@gmail.com>
2024-09-18 22:18:07 +0530

    Product page add to cart function developed

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -2,9 +2,12 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagement;
+use App\Livewire\Partials\Navbar;
 use App\Models\Brand;
 use App\Models\Category;
 use App\Models\Product;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Attributes\Title;
 use Livewire\Attributes\Url;
 use Livewire\Component;
@@ -14,6 +17,8 @@ use Livewire\WithPagination;
 
 class ProductsPage extends Component {
 
+    use LivewireAlert;
+    
     use WithPagination;
 
     #[Url()]
@@ -32,6 +37,32 @@ class ProductsPage extends Component {
     public $price_range = 0;
 
     public $sort = 'latest';
+
+
+
+    // ..::ADD PRODUCT TO CART METHOD::..
+
+    // This method adds a product to the cart and then updates the cart count in the navbar
+    public function addToCart($product_id) {
+        // Get the total count of items in the cart after adding the product
+        $total_count = CartManagement::addItemToCart($product_id);
+
+        // Dispatch an event to the navbar component to update the cart count
+        // This is done using the Livewire `dispatch` method.
+        $this->dispatch('update-cart-count', total_count: $total_count)->to(Navbar::class);
+
+        // Show a success message to the user that the product has been added to the cart
+        // The message is shown at the bottom end of the screen and disappears after 3 seconds.
+        $this->alert('success', 'Product added to cart successfully!', [
+            'position' => 'bottom-end',
+            'timer' => 3000,
+            'toast' => true,
+        ]);
+    }
+    
+    // ..::END ADD PRODUCT TO CART METHOD::..
+
+
 
     public function render() {
         $productQuery = Product::query()->where('is_active', 1);
